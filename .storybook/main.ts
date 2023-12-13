1ac5d3b680ce5f3e6db6a64a2a1e3af4cf239a30
Emiya·Leon <wangs.coder@gmail.com>
2023-12-13 09:24:14 +0800

    feat: init

--- .storybook/main.ts
+++ .storybook/main.ts
@@ -3,7 +3,7 @@ import type { StorybookConfig } from '@modern-js/storybook';
 
 const config: StorybookConfig = {
   stories: ['../stories/**/*.stories.@(js|jsx|ts|tsx)'],
-  addons: ['@storybook/addon-essentials', '@storybook/addon-styling-webpack'],
+  addons: ['@storybook/addon-essentials'],
   framework: {
     name: '@modern-js/storybook',
     options: {
