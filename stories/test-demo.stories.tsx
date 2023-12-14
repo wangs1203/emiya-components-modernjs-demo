39e9f210e66975bf761f561fb72264768dc908d9
Emiya·Leon <wangs.coder@gmail.com>
2023-12-14 16:21:25 +0800

    style: delete useless comments

--- stories/test-demo.stories.tsx
+++ stories/test-demo.stories.tsx
@@ -1,11 +1,9 @@
-// import React from 'react';
 // import Emiya from 'emiya-components';
 import { Meta, StoryObj } from '@storybook/react';
 import { TestDemo } from '../src';
 
 // console.log(Emiya);
 
-// export const Component = () => <TestDemo />;
 const storyMeta: Meta<typeof TestDemo> = {
   title: 'Components/TestDemo',
   component: TestDemo,
