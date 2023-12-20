6e2f8458bc9a2c0fc38760212ef0d0030003bd5b
Emiya·Leon <wangs.coder@gmail.com>
2023-12-20 15:30:14 +0800

    feat(component): update container

--- src/container/index.tsx
+++ src/container/index.tsx
@@ -1,6 +1,5 @@
 import type { HTMLAttributes, ReactNode } from 'react';
 import clsx from 'clsx';
-import styles from './container.module.less';
 
 export interface IContainerProps extends HTMLAttributes<HTMLDivElement> {
   children: ReactNode;
@@ -18,7 +17,6 @@ export default function Container({
         `tw-flex tw-flex-col tw-overflow-hidden tw-h-full${
           full ? ' tw-p-5' : ''
         }`,
-        styles.container,
       ])}
       {...props}
     >
