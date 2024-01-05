537b0c60e688285d63cc9bb26976d4e976197988
Emiya·Leon <wangs.coder@gmail.com>
2024-01-05 14:13:09 +0800

    feat(component): Making modifications to Container component

--- src/container/index.tsx
+++ src/container/index.tsx
@@ -8,19 +8,24 @@ export interface IContainerProps extends HTMLAttributes<HTMLDivElement> {
 
 export default function Container({
   children,
-  full,
+  full = false,
   ...props
 }: IContainerProps) {
   return (
     <div
-      className={clsx([
-        `tw-flex tw-flex-col tw-overflow-hidden tw-h-full${
-          full ? ' tw-p-5' : ''
-        }`,
-      ])}
-      {...props}
+      className={`tw-overflow-hidden tw-flex-auto tw-min-h-0${
+        !full ? ' tw-p-5 tw-pb-1' : ''
+      }`}
     >
-      {children}
+      <div
+        className={clsx(
+          'tw-flex tw-flex-col tw-overflow-hidden tw-h-full tw-relative tw-bg-white tw-p-5',
+          { 'tw-rounded-md tw-shadow-[0_0_6px_#a1a0a0]': !full },
+        )}
+        {...props}
+      >
+        {children}
+      </div>
     </div>
   );
 }
